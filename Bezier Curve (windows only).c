acb43d9d75dbcd7dd59302d2a3fc4170c1280c87
Ashish Gaikwad <ash.gkwd@gmail.com>
2014-04-26 19:59:30 +0530

    Bezier curve drawing algorithm is updated. And Polygon Filling Scanline algo added. Windows only version

--- Bezier Curve (windows only).c
+++ Bezier Curve (windows only).c
@@ -1,3 +1,9 @@
+/**
+* @author: Ashish A Gaikwad <ash.gkwd@gmail.com>
+* Bezier Curve Drawing Algorithm in C. 
+* Runs only on windows due to dependency on 'graphics.h' header file.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <graphics.h>
